36ee04a3db2419c737edb1a260476d02eab14be1
Oleksandr Yevdokymov <o.yevdokymov@khai.edu>
2022-11-20 20:09:09 +0200

    Add initial project structure: add Spring Boot, add Vaadin

---
+++ src/test/java/edu/khai/algorithms/sorting/SortingAlgorithmsApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.khai.algorithms.sorting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SortingAlgorithmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
