daff5cd980b0a9692ec08a53f0a840454e57e207
Oleksandr Yevdokymov <o.yevdokymov@khai.edu>
2022-11-21 01:13:40 +0200

    Add sample line chart

---
+++ src/main/java/edu/khai/algorithms/sorting/view/MainView.java
@@ -0,0 +1,46 @@
+package edu.khai.algorithms.sorting.view;
+
+import com.github.appreciated.apexcharts.ApexCharts;
+import com.github.appreciated.apexcharts.ApexChartsBuilder;
+import com.github.appreciated.apexcharts.config.builder.*;
+import com.github.appreciated.apexcharts.config.chart.Type;
+import com.github.appreciated.apexcharts.config.chart.builder.ZoomBuilder;
+import com.github.appreciated.apexcharts.config.grid.builder.RowBuilder;
+import com.github.appreciated.apexcharts.config.stroke.Curve;
+import com.github.appreciated.apexcharts.config.subtitle.Align;
+import com.github.appreciated.apexcharts.helper.Series;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.Route;
+
+@Route("")
+public class MainView extends VerticalLayout {
+
+    public MainView() {
+        ApexCharts lineChart = ApexChartsBuilder.get()
+                .withChart(ChartBuilder.get()
+                        .withType(Type.LINE)
+                        .withZoom(ZoomBuilder.get()
+                                .withEnabled(false)
+                                .build())
+                        .build())
+                .withStroke(StrokeBuilder.get()
+                        .withCurve(Curve.STRAIGHT)
+                        .build())
+                .withTitle(TitleSubtitleBuilder.get()
+                        .withText("Product Trends by Month")
+                        .withAlign(Align.LEFT)
+                        .build())
+                .withGrid(GridBuilder.get()
+                        .withRow(RowBuilder.get()
+                                .withColors("#f3f3f3", "transparent")
+                                .withOpacity(0.5).build()
+                        ).build())
+                .withXaxis(XAxisBuilder.get()
+                        .withCategories("Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep")
+                        .build())
+                .withSeries(new Series<>("Desktops", 10.0, 41.0, 35.0, 51.0, 49.0, 62.0, 69.0, 91.0, 148.0))
+                .build();
+        add(lineChart);
+        setWidth("100%");
+    }
+}
